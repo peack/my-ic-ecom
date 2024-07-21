92fc3f62ad94d6f6151780111c160aff60fab490
peack <poliouxx@gmail.com>
2024-07-21 17:28:30 +0200

    fixes linting. builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  extends: ['@payloadcms'],
+  extends: ['@payloadcms', 'plugin:@next/next/recommended'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  "rules": {
+    "import/no-default-export": "off"
+  }
 }
