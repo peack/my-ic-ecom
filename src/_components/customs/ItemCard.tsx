e2c6cc852eaefe03efc7e6c9d693b044f8f6e4ad
peack <poliouxx@gmail.com>
2024-07-25 17:29:44 +0200

    add icons lib and update card to link on header

--- src/_components/customs/ItemCard.tsx
+++ src/_components/customs/ItemCard.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card"
 import Image from "next/image"
 import { Product } from "@/payload/payload-types"
+import Link from "next/link"
 
 interface ItemCardProps {
     slug: string
@@ -10,6 +11,7 @@ interface ItemCardProps {
 const ItemCard:React.FC<ItemCardProps> = (({ slug , product}) => {
     return (
         <Card className="w-[300px]" key={slug}>
+            <Link key={product.slug} href={`/products/${product.slug}`}>
             <CardHeader className="flex items-center space-x-4  p-4">
             <Image
              src={ product.meta?.image?.url ?? "/Image_NA.png"}
@@ -20,11 +22,11 @@ const ItemCard:React.FC<ItemCardProps> = (({ slug , product}) => {
              />
              <span className="font-bold">{product.title}</span>
             </CardHeader>
+            </Link>
             <CardContent>
             <p>{product.meta?.description }</p>
             </CardContent>
-            <CardFooter>
-            <p>Icon here</p>
+            <CardFooter className="flex justify-end p-4">
             </CardFooter>
         </Card>
     )
--- src/_components/customs/ProductList.tsx
+++ src/_components/customs/ProductList.tsx
@@ -2,7 +2,6 @@
 
 import { useProducts } from "@/_hooks/useProducts";
 import ItemCard from "./ItemCard";
-import Link from "next/link";
 
 const  ProductList: React.FC = () => {
   const { products, loading, error } = useProducts();
@@ -12,9 +11,7 @@ const  ProductList: React.FC = () => {
             { loading ? <p>Loading...</p> : error ? <p>Error: {error}</p> :
                 <div className="flex flex-wrap justify-center md:justify-start">
                 {products.map((product) => (
-                        <Link key={product.slug} href={`/products/${product.slug}`}>
                           <ItemCard product={product} slug={product.slug}/>
-                        </Link>                          
                 ))}
                 </div>
             }
