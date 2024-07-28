c920d553cf49649d3a60900ce89a442b955a1adc
peack <poliouxx@gmail.com>
2024-07-28 23:23:36 +0200

    updates header to add user item

--- src/_components/customs/Header.tsx
+++ src/_components/customs/Header.tsx
@@ -20,24 +20,34 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ slug, navLinks }) => {
   return (
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 ">
-            <NavigationMenu>
-              <NavigationMenuList >
-                {navLinks.map((link) => (
-                  <NavigationMenuItem key={link} >
-                    <Link
-                      href={`/${link.toLowerCase() || ""}`}
-                      className={navigationMenuTriggerStyle() + link === 'Admin' ? 'flex-row-reverse' :'aa'  }
-                    >
-                      {link}
-                    </Link>
-                  </NavigationMenuItem>
-                ))}
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
-        </header>
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 ">
+        <div className="container mx-auto flex justify-between items-center">
+          <NavigationMenu>
+            <NavigationMenuList className="">
+              {navLinks.map(link => (
+                <NavigationMenuItem key={link}>
+                  <Link
+                    href={`/${link.toLowerCase() || ''}`}
+                    className={
+                      navigationMenuTriggerStyle() + link === 'Admin' ? 'flex-row-reverse' : 'aa'
+                    }
+                  >
+                    {link}
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex items-center"></div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem key={'user'}>User</NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+    </header>
   )
 }
 
